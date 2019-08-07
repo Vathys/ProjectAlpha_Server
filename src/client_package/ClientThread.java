36ad856f61a96b9c52c1db21ce5bee64762f674f
Vathys <deepdesai00@gmail.com>
2019-08-06 20:54:33 -0500

    Minor Change

--- src/client_package/ClientThread.java
+++ src/client_package/ClientThread.java
@@ -24,6 +24,7 @@ public class ClientThread extends Thread
      {
           this.client = client;
           clientCom = new ConcurrentLinkedQueue<String>();
+          writer = new ThreadWriter();
      }
 
      public void startThreads()
